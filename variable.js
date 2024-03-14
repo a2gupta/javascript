d34eb0cd66ddd6ef386b630df641944270f33a06
a2gupta <163423423+a2gupta@users.noreply.github.com>
2024-03-14 19:26:07 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,6 @@
+const accountid = 144553
+let accountemail = "hitesh@google.com"
+var accountcitypassword = "12345"
+accountcity = "jaipur"
+console.log(accountid);
+console.table([accountcitypassword,accountemail,accountid,accountcity])
